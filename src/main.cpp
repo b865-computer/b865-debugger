a9095fa9afcac93fd2540d61b6c4fac87298eb77
Rbel12b <150368027+Rbel12b@users.noreply.github.com>
2024-08-31 19:20:49 +0200

    update loop

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,11 @@ int main(int argc, char *argv[])
             }
         }
         std::this_thread::sleep_for(std::chrono::nanoseconds(frequency.sleep));
+        counter++;
+        if(counter == frequency.HZ * 10)
+        {
+            break;
+        }
     }
     return 0;
 }
